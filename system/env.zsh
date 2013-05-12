4eccedf1e1cdb5ba0273014131f04f4be688e82a
José Tomás Albornoz <jojo@eljojo.net>
2013-05-12 13:02:56 +0200

    added environment variables for amazon thingy

--- system/env.zsh
+++ system/env.zsh
@@ -1,3 +1,8 @@
 export EDITOR='vim'
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
+
+export JAVA_HOME="$(/usr/libexec/java_home)"
+export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
+export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
+export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
