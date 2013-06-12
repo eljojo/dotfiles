68900fe9ee9025cb2ed1242bcdf4e46d236a98f3
José Tomás Albornoz <jojo@eljojo.net>
2013-06-12 23:41:22 +0200

    Added support for brew installed Ruby (in PATH)

--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,9 @@
-export PATH='/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin'
-
+export PATH='/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin'
 export PATH="./bin:$HOME/.rbenv/shims:$HOME/.rbenv/bin:/usr/local/share/npm/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+if [[ -a /usr/local/opt/ruby/bin ]]
+then
+  export PATH="/usr/local/opt/ruby/bin:$PATH"
+fi
