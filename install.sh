85613cf86122aeae28b88be5a3c6e1af3ac0ac43
José Albornoz <jojo@eljojo.net>
2014-10-12 12:52:48 +0200

    Fixed install process

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 cd ~/.vim
-git submodule init
-git submodule update
-git submodule foreach git checkout master
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/gvimrc ~/.gvimrc
-
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
