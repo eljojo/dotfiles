dc316bc07e506c18d8c7db9db3534aa38ee6c6cd
José Tomás Albornoz <jojo@eljojo.net>
2014-10-12 13:34:10 +0200

    Added support for chruby

---
+++ ruby/chruby.zsh
@@ -0,0 +1,10 @@
+# there was a rbenv thing here.
+if [ "$(uname -s)" = "Darwin" ]
+then
+  export RUBY_GC_MALLOC_LIMIT=60000000
+  export RUBY_GC_HEAP_FREE_SLOTS=200000
+fi
+
+source /usr/local/opt/chruby/share/chruby/chruby.sh
+source /usr/local/opt/chruby/share/chruby/auto.sh
+
