b9e9fbc654bd15914c030306d17cb50f6a95b61a
José Tomás Albornoz <jojo@eljojo.net>
2014-11-13 18:12:03 +0100

    added google cloud completion thingy.
    i'll probably revert this at some point

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,5 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+source "$HOME/google-cloud-sdk/completion.zsh.inc"
