354153711a92e3ce8cb505c4ba6eba3d81ca75d5
José Tomás Albornoz <jojo@eljojo.net>
2015-02-10 00:28:54 +0100

    Commented google stuff that fails in linux

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -4,4 +4,4 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
 
-source "$HOME/google-cloud-sdk/completion.zsh.inc"
+#source "$HOME/google-cloud-sdk/completion.zsh.inc"
