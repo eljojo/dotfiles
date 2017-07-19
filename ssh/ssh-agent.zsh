1c83af8127fae7bf22c86bf3ff270b143f1a983c
José Albornoz <jojo@eljojo.net>
2017-07-18 23:51:03 -0400

    improve linux ssh agent

--- ssh/ssh-agent.zsh
+++ ssh/ssh-agent.zsh
@@ -1,7 +1,12 @@
 # https://wiki.archlinux.org/index.php/SSH_keys#SSH_agents
-if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+
+if [ -S "/run/user/$UID/ssh-agent.socket" ]; then
+  SSH_AUTH_SOCK=/run/user/$UID/ssh-agent.socket; export SSH_AUTH_SOCK;
+else
+  if ! pgrep -u "$USER" ssh-agent > /dev/null; then
     ssh-agent > ~/.ssh-agent-daemon
-fi
-if [[ "$SSH_AGENT_PID" == "" ]]; then
+  fi
+  if [[ "$SSH_AGENT_PID" == "" ]]; then
     eval "$(<~/.ssh-agent-daemon)" > /dev/null
+  fi
 fi
