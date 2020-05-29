9c886f305a276f643f01569ebe605e3d5616e143
José Albornoz <jojo@eljojo.net>
2020-05-29 00:04:31 -0400

    update mac script

--- install_stuff_mac.sh
+++ install_stuff_mac.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
- echo "> installing Firefox"
- curl -Lo /tmp/Firefox.dmg "https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US";
- hdiutil attach /tmp/Firefox.dmg;
- ditto -rsrc /Volumes/Firefox/Firefox.app /Applications/Firefox.app;
- hdiutil detach /Volumes/Firefox;
- rm /tmp/Firefox.dmg;
- defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Firefox.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
- 
- echo "> installing Spotify"
- curl -Lo /tmp/spotify.dmg https://download.scdn.co/Spotify.dmg;
- hdiutil attach /tmp/spotify.dmg;
- ditto -rsrc /Volumes/Spotify/Spotify.app /Applications/Spotify.app;
- hdiutil detach /Volumes/Spotify;
- rm /tmp/spotify.dmg;
+echo "> installing Firefox"
+curl -Lo /tmp/Firefox.dmg "https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US";
+hdiutil attach /tmp/Firefox.dmg;
+ditto -rsrc /Volumes/Firefox/Firefox.app /Applications/Firefox.app;
+hdiutil detach /Volumes/Firefox;
+rm /tmp/Firefox.dmg;
+defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Firefox.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+
+echo "> installing Spotify"
+curl -Lo /tmp/spotify.dmg https://download.scdn.co/Spotify.dmg;
+hdiutil attach /tmp/spotify.dmg;
+ditto -rsrc /Volumes/Spotify/Spotify.app /Applications/Spotify.app;
+hdiutil detach /Volumes/Spotify;
+rm /tmp/spotify.dmg;
 defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Spotify.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
 
+echo "> installing Roon"
+curl -Lo /tmp/Roon.dmg "http://download.roonlabs.com/builds/Roon.dmg";
+hdiutil attach /tmp/Roon.dmg;
+ditto -rsrc /Volumes/Roon/Roon.app /Applications/Roon.app;
+hdiutil detach /Volumes/Roon;
+rm /tmp/Roon.dmg;
+defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Roon.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+
 echo "> installing MacVim"
-curl -Lo /tmp/macvim.dmg https://github.com/macvim-dev/macvim/releases/download/snapshot-153/MacVim.dmg;
-hdiutil attach /tmp/macvim.dmg;
-ditto -rsrc /Volumes/MacVim/MacVim.app /Applications/MacVim.app;
-hdiutil detach /Volumes/MacVim;
-rm /tmp/macvim.dmg;
+brew cask install macvim
 defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/MacVim.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
 
 echo "> installing 1Password from mac app store"
@@ -30,6 +34,7 @@ defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</
 
 echo "> installing Tweetbot 3 from mac app store"
 mas install 1384080005
+defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Tweetbot.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
 
 echo "> installing iTerm 2"
 curl -Lo /tmp/iterm2.zip https://iterm2.com/downloads/stable/iTerm2-3_3_6.zip;
@@ -38,11 +43,14 @@ mv /tmp/iTerm.app /Applications/iTerm.app
 rm /tmp/iterm2.zip
 defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/iTerm.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
 
-brew cask install flycut
 echo "> installing Flycut"
-curl -Lo /tmp/flycut.zip https://github.com/TermiT/Flycut/releases/download/1.9.4/Flycut.app.1.9.4.zip;
-unzip /tmp/flycut.zip -d /tmp/
-mv /tmp/Flycut.app /Applications/Flycut.app
-rm /tmp/flycut.zip
+brew cask install flycut
+
+echo "> installing Syncthing"
+curl -Lo /tmp/Syncthing.dmg "https://github.com/syncthing/syncthing-macos/releases/download/v1.0.0-2/Syncthing-1.0.0-2.dmg";
+hdiutil attach /tmp/Syncthing.dmg;
+ditto -rsrc /Volumes/Syncthing/Syncthing.app /Applications/Syncthing.app;
+hdiutil detach /Volumes/Syncthing;
+rm /tmp/Syncthing.dmg;
 
 killall Dock
