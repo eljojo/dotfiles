3f5d0144b5af05a3f28942c49facbc56dd1b939c
José Albornoz <jojo@eljojo.net>
2022-09-09 14:25:06 -0400

    less defaults for mac, leaner setup relying on nix-darwin

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -9,142 +9,59 @@
 #
 # Run ./set-defaults.sh and you'll be good to go.
 
-# Keyboard: Disable press-and-hold for keys in favor of key repeat.
-defaults write -g ApplePressAndHoldEnabled -bool false
-
-# Keyboard: Set a really fast key repeat.
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 25
-
 # Set the Finder prefs for showing a few different volumes on the Desktop.
 # defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 # defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
-# Locale: METRIC SYSTEM! - broken
-# defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
-# defaults write NSGlobalDomain AppleMetricUnits -bool true
+# Locale: 24-hour time
+defaults write NSGlobalDomain AppleICUForce24HourTime -bool true
 
-# Locale: 24-hour time - broken
-# defaults write NSGlobalDomain AppleICUForce12HourTime -bool false
-
-# Calendar: Week starts on monday
+# Calendar: Week starts on Monday
 defaults write com.apple.iCal "first day of week" -int 1
-
-# Hot corners
-# Possible values:
-#  0: no-op
-#  2: Mission Control
-#  3: Show application windows
-#  4: Desktop
-#  5: Start screen saver
-#  6: Disable screen saver
-#  7: Dashboard
-# 10: Put display to sleep
-# 11: Launchpad
-# 12: Notification Center
-
-# Bottom left screen corner → Desktop
-defaults write com.apple.dock wvous-bl-corner -int 4
-defaults write com.apple.dock wvous-bl-modifier -int 0
-
-# Bottom right corner → put display to sleep
-# defaults write com.apple.dock wvous-br-corner -int 10
-# defaults write com.apple.dock wvous-br-modifier -int 0
 
 # Top left corner → Mission Control
 defaults write com.apple.dock wvous-tl-corner -int 2
 defaults write com.apple.dock wvous-ll-modifier -int 0
 
-# Expand save panel by default
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
-
-# Expand print panel by default
-# defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-# defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
-
-# "Automatically quit printer app once the print jobs complete"
-# defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
-
-# "Saving to disk (not to iCloud) by default"
-# defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
-
-# Disable the “Are you sure you want to open this application?” dialog
-defaults write com.apple.LaunchServices LSQuarantine -bool false
-
 # Disable Resume system-wide
-# defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
+defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
 
 # "Remove duplicates in the "Open With" menu"
 /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
 
-# "Disable smart quotes and smart dashes as they're annoying when typing code"
-defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
-defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
-# TODO: add this for autofix and autocapitalization
-
-# Trackpad: enable tap to click for this user and for the login screen
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-
 # Trackpad: Swipe between full-screen apps
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -int 2
 
-# Trackpad: Enable other multi-finger gestures
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerVertSwipeGesture -int 2
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerVertSwipeGesture -int 2
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerPinchGesture -int 2
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerHorizSwipeGesture -int 2
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFiveFingerPinchGesture -int 2
-
-defaults write com.apple.dock showMissionControlGestureEnabled -bool true
 defaults write com.apple.dock showAppExposeGestureEnabled -bool true
-defaults write com.apple.dock showDesktopGestureEnabled -bool true
-defaults write com.apple.dock showLaunchpadGestureEnabled -bool true
+# defaults write com.apple.dock showMissionControlGestureEnabled -bool true
+# defaults write com.apple.dock showDesktopGestureEnabled -bool true
+# defaults write com.apple.dock showLaunchpadGestureEnabled -bool true
 
 # Use scroll gesture with the Ctrl (^) modifier key to zoom - https://developer.apple.com/documentation/devicemanagement/accessibility
-# broken?
 sudo defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
-defaults write com.apple.AppleMultitouchTrackpad HIDScrollZoomModifierMask -int 262144
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad HIDScrollZoomModifierMask -int 262144
-
-# "Enabling full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
-defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
-
-# "Showing status bar in Finder by default"
-defaults write com.apple.finder ShowStatusBar -bool true
-
-# Finder: show path bar
-defaults write com.apple.finder ShowPathbar -bool true
 
 # Finder: New window points to home
 defaults write com.apple.finder NewWindowTarget -string "PfHm"
 
-# Finder: Disable the warning when changing a file extension
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-
-# QuickLook: "Allowing text selection in Quick Look/Preview in Finder by default"
-defaults write com.apple.finder QLEnableTextSelection -bool true
-
 # Finder: "Avoiding the creation of .DS_Store files on network volumes"
+# https://support.apple.com/en-ca/HT208209
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-# Desktop: Enabling snap-to-grid for icons on the desktop
-# /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-# TODO: use stacks and group by kind
+# Desktop
+defaults write com.apple.Finder FXPreferredGroupBy Kind # group desktop icons by kind
+# TODO: use stacks
 
 # Dock: autohiding dock
 # defaults write com.apple.dock autohide -bool true
 
 # Dock: Setting the icon size of Dock items to 36 pixels for optimal size/screen-realestate"
-defaults write com.apple.dock tilesize -int 30
+# defaults write com.apple.dock tilesize -int 30
 
 # Dock: enable dock magnification
-defaults write com.apple.dock magnification -bool true
+# defaults write com.apple.dock magnification -bool true
 
 # Dock: magnified size of dock icons
-defaults write com.apple.dock largesize -int 100
+# defaults write com.apple.dock largesize -int 100
 
 # Mail: "Setting email addresses to copy as 'foo@example.com' instead of 'Foo Bar <foo@example.com>' in Mail.app"
 defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
@@ -157,14 +74,11 @@ defaults write com.apple.Safari SuppressSearchSuggestions -bool true
 defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
 
 # Safari: Show the full URL in the address bar (note: this still hides the scheme)
-defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
+# defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
 
 # Safari: Press Tab to highlight each item on a web page
-defaults write com.apple.Safari WebKitTabToLinksPreferenceKey -bool true
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2TabsToLinks -bool true
-
-# Safari: Warn about fraudulent websites
-defaults write com.apple.Safari WarnAboutFraudulentWebsites -bool true
+# defaults write com.apple.Safari WebKitTabToLinksPreferenceKey -bool true
+# defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2TabsToLinks -bool true
 
 # Safari: Enable “Do Not Track”
 defaults write com.apple.Safari SendDoNotTrackHTTPHeader -bool true
@@ -179,52 +93,15 @@ defaults write com.apple.terminal SecureKeyboardEntry -bool true
 # Time Machine: Prevent Time Machine from prompting to use new hard drives as backup volume
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
-# Activity Monitor: Show all processes in Activity Monitor
-defaults write com.apple.ActivityMonitor ShowCategory -int 0
-# Activity Monitor: Sort Activity Monitor results by CPU usage
-defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
-defaults write com.apple.ActivityMonitor SortDirection -int 0
-
 # Photos: Prevent Photos from opening automatically when devices are plugged in
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 
-# Messages: Disable smart quotes as it’s annoying for messages that contain code
+# Messages: Disable smart quotes
 defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticQuoteSubstitutionEnabled" -bool false
 
 # Tweetbot: Bypass the annoyingly slow t.co URL shortener
 defaults write com.tapbots.TweetbotMac OpenURLsDirectly -bool true
 
-# Restart automatically if the computer freezes
-# sudo systemsetup -setrestartfreeze on
-
 # Finder: Default view for new windows
 # defaults write com.apple.finder FXPreferredViewStyle Clmv # for column view
 # defaults write com.apple.Finder FXPreferredViewStyle Nlsv # for list view
-
-# Map Caps Lock key to Left Control key - seems to be broken
-# https://developer.apple.com/library/content/technotes/tn2450/_index.html
-# hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E0}]}'
-
-# "Speeding up wake from sleep from 1 hour to 8 hours"
-# http://www.cultofmac.com/221392/quick-hack-speeds-up-retina-macbooks-wake-from-sleep-os-x-tips/
-# sudo pmset -a standbydelay 28800
-
-# Finder: Show the ~/Library folder.
-# chflags nohidden ~/Library
-
-# "Increasing sound quality for Bluetooth headphones/headsets"
-# defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
-
-# "Turn off keyboard illumination when computer is not used for 5 minutes"
-# defaults write com.apple.BezelServices kDimTime -int 300
-
-# "Requiring password immediately after sleep or screen saver begins"
-# defaults write com.apple.screensaver askForPassword -int 1
-# defaults write com.apple.screensaver askForPasswordDelay -int 5
-
-# "Enabling subpixel font rendering on non-Apple LCDs"
-# https://github.com/kevinSuttle/macOS-Defaults/issues/17#issuecomment-266633501
-# defaults write NSGlobalDomain AppleFontSmoothing -int 1
-
-# Enable HiDPI display modes (requires restart)
-# sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
