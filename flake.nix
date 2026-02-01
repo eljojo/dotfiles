f39e3c2be97468394f9b53d72fd0847c5397bd5e
José Albornoz <jojo@eljojo.net>
2026-02-01 06:08:57 -0500

    try using 25.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "jojo's dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
